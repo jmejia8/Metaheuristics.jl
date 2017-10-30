4e25fdb65bc311be29f170fe374c2637777a3ef4
Jesús Mejía <jesusmejded@gmail.com>
2017-10-29 20:33:36 -0600

    PSO algorithm added

--- src/Metaheuristics.jl
+++ src/Metaheuristics.jl
@@ -1,12 +1,15 @@
 module Metaheuristics
 
 # ECA algorithm
-export eca, diffEvolution
+export eca, diffEvolution, pso
 
 # ECA algorithm
 include("eca.jl")
 
 # Differential algorithm
 include("diffEvolution.jl")
+
+# PSO algorithm
+include("pso.jl")
 
 end # module
