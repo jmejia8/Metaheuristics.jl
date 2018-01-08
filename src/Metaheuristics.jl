ec1434f12111ac9f8fd873d07dc6459834229229
Jesús Mejía <jesusmejded@gmail.com>
2018-01-07 22:05:26 -0600

    exporting res

--- src/Metaheuristics.jl
+++ src/Metaheuristics.jl
@@ -2,10 +2,11 @@ __precompile__()
 module Metaheuristics
 
 # ECA algorithm
-export eca, diffEvolution, pso
+export eca, diffEvolution, pso, ecaConstrained
 
 # ECA algorithm
 include("eca.jl")
+include("ecaConstrained.jl")
 
 # Differential algorithm
 include("diffEvolution.jl")
