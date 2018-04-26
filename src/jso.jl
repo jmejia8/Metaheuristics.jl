caf045e9271918d9dba9ec7e1ea77595e90bdc3e
Jesús Mejía <jesusmejded@gmail.com>
2018-04-26 11:36:19 -0500

    using is_better

--- src/jso.jl
+++ src/jso.jl
@@ -9,7 +9,7 @@ function getFw(F::Float64, nfes::Int, max_nfes::Int)
 end
 
 function getPbests(P::Array, p::Float64)
-    i = sortperm(P, lt=Selection, rev=true)
+    i = sortperm(P, lt=is_better)
     N = length(P)
 
     n = round(Int, p*N)
@@ -253,7 +253,7 @@ function jso(fobj::Function, D::Int;
         end
 
         if  N < N_old
-            Ids = sortperm(population, lt=Selection, rev=true)[1:N]
+            Ids = sortperm(population, lt=is_better)[1:N]
 
             population = population[Ids]
             P = P[Ids,:]
