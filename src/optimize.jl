6e00fbb53cd2bc85b8f0cb8f4f32b2f4aa718a74
Jesus Mejia <jesusmejded@gmail.com>
2019-08-29 14:25:30 -0500

    optimize framework

---
+++ src/optimize.jl
@@ -0,0 +1,54 @@
+function optimize(f::Function, # objective function
+                  bounds::Array,
+                  method::Algorithm
+                  )
+
+      problem = Problem(f, bounds)
+      engine = method.engine
+
+      method.options.debug && println("Initializing population...")
+      engine.initialize!(problem, engine, method.parameters, method.status, method.information, method.options)
+
+      #####################################
+      # common methods
+      #####################################
+      status = method.status
+      information = method.information
+      options     = method.options
+      update_state! = engine.update_state!
+      final_stage!  = engine.final_stage!
+      ###################################
+
+      ###################################
+      # store convergence
+      ###################################
+      if options.store_convergence
+            st = deepcopy(status)
+            empty!(st.convergence)
+            push!(status.convergence, st)
+      end
+      
+      method.options.debug && println("Starting main loop...")
+
+      status.iteration = 0
+      while !engine.stop_criteria(status, information, options)
+            status.iteration += 1
+
+            update_state!(problem,engine,method.parameters,method.status,method.information,method.options,status.iteration)
+            
+            options.debug && display(status)
+
+
+            if options.store_convergence
+                  st = deepcopy(status)
+                  empty!(st.convergence)
+                  push!(status.convergence, st)
+            end
+            
+      end
+
+      final_stage!(status, information, options)
+
+      return status
+
+end
