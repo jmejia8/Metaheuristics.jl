322b0ad0d85bc6f193b244557f10a2632470ab22
Jesús Mejía <jesusmejded@gmail.com>
2020-01-15 22:37:18 -0600

    minor changes

--- src/tools.jl
+++ src/tools.jl
@@ -179,7 +179,8 @@ function violationsSum(g::Vector, h::Vector; ε =0.0)
 
     for i = 1:length(h)
         if ≈(h[i], 0.0, atol=ε)
-            sum_h += abs(h[i])  end
+            sum_h += abs(h[i])
+        end
     end
 
     return (sum_g/length(g)) + (sum_h / length(h)) 
