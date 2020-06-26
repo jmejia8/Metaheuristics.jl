070152ab8c5cc4dec53c2c1d45f8262374af2f3a
Jesus Mejia <jesusmejded@gmail.com>
2020-06-25 22:03:20 -0500

    Format code

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,24 +1,32 @@
-function optimize(f::Function, # objective function
-                  bounds::Array,
-                  method::AbstractAlgorithm
-                  )
+function optimize(
+      f::Function, # objective function
+      bounds::AbstractMatrix,
+      method::AbstractAlgorithm,
+)
 
-      problem = Problem(f, bounds)
+      problem = Problem(f, Array(bounds))
       engine = method.engine
       convergence = State[]
       method.options.debug && @info("Initializing population...")
 
       method.status.start_time = time()
-      engine.initialize!(problem, engine, method.parameters, method.status, method.information, method.options)
+      engine.initialize!(
+            problem,
+            engine,
+            method.parameters,
+            method.status,
+            method.information,
+            method.options,
+      )
 
       #####################################
       # common methods
       #####################################
       status = method.status
       information = method.information
-      options     = method.options
+      options = method.options
       update_state! = engine.update_state!
-      final_stage!  = engine.final_stage!
+      final_stage! = engine.final_stage!
       ###################################
 
       ###################################
@@ -27,15 +35,23 @@ function optimize(f::Function, # objective function
       if options.store_convergence
             update_convergence!(convergence, status)
       end
-      
+
       method.options.debug && @info("Starting main loop...")
 
       status.iteration = 0
       while !status.stop
             status.iteration += 1
 
-            update_state!(problem,engine,method.parameters,method.status,method.information,method.options,status.iteration)
-            
+            update_state!(
+                  problem,
+                  engine,
+                  method.parameters,
+                  method.status,
+                  method.information,
+                  method.options,
+                  status.iteration,
+            )
+
             if options.debug
                   status.final_time = time()
                   display(status)
@@ -44,7 +60,7 @@ function optimize(f::Function, # objective function
             if options.store_convergence
                   update_convergence!(convergence, status)
             end
-            
+
       end
 
       final_stage!(status, information, options)
