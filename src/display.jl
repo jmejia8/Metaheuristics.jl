94bd917165de2368b12d40c72c16c29153021218
Jesús Mejía <jesusmejded@gmail.com>
2020-09-28 22:42:52 -0500

    Fix display function to show fancy reports

--- src/display.jl
+++ src/display.jl
@@ -13,7 +13,7 @@ function display(solution::xfgh_indiv)
     println(solution.g)
     @printf("| h(x) = ")
     println(solution.h)
-    @show(solution.x)
+    println("| x = ", solution.x)
 
 
 end
@@ -21,7 +21,7 @@ end
 function display(population::Array{xFgh_indiv})
     x = map(s -> s.f[1], population)
     y = map(s -> s.f[2], population)
-    plt = scatterplot(x, y, title="Population", xlabel="f_1", ylabel="f_2")
+    plt = scatterplot(x, y, title="F space", xlabel="f_1", ylabel="f_2")
     display(plt)
 end
 
@@ -31,12 +31,31 @@ function display(status::State)
     #     return println(status)
     # end
 
-    println("+=========== STATE ==========+")
+    println("+=========== RESULT ==========+")
     @printf("| Iter.: %.0f\n", status.iteration)
-    display(status.population)
+
+    if typeof(status.best_sol) <: xf_indiv || typeof(status.best_sol) <: xfgh_indiv
+        display(status.best_sol)
+
+    elseif typeof(Array(status.population)) <: Array{xFgh_indiv}
+        println("Population")
+        display(Array(status.population))
+
+        b = nothing
+        try
+            b = Array(status.best_sol)
+        catch
+            b = nothing
+        end
+        if !isnothing(b)
+            println("\nBest solution(s)")
+            display(b)
+        end
+        print("\n")
+    end
 
 
-    @printf("\n| f calls: %.0f\n", status.f_calls)
+    @printf("| f calls: %.0f\n", status.f_calls)
     @printf("| Total time: %.4f s\n", status.final_time - status.start_time)
     println("+============================+")
 end
