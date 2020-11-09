0b1bb6cb2622b1dcd46abeae06ffd631cf6b0821
Jesús Mejía <jesusmejded@gmail.com>
2020-11-09 01:12:35 -0600

    bound constraint handling

--- src/tools.jl
+++ src/tools.jl
@@ -5,6 +5,74 @@ struct Individual_
     h::Vector{Float64}
 end
 
+
+
+function reflected_back_to_bound!(x, bounds)
+    for i in 1:size(bounds,2)
+        l = bounds[1, i]
+        u = bounds[2, i]
+        while !( l <= x[i] <= u )
+            x[i] = x[i] < l ? 2l - x[i] : 2u - x[i]
+        end
+
+    end
+    x
+end
+
+function replace_with_random_in_bounds!(x, bounds) 
+    for i in 1:size(bounds,2)
+        l = bounds[1, i]
+        u = bounds[2, i]
+        if !( l <= x[i] <= u )
+            x[i] = l + rand() * (u - l)
+        end
+
+    end
+    x
+end
+
+function wrap_to_bounds!(x, bounds)
+    for i in 1:size(bounds,2)
+        l = bounds[1, i]
+        u = bounds[2, i]
+        if !( l <= x[i] <= u )
+            ρ = u - l
+            x[i] = x[i] < l ? u - (l - x[i]) % ρ : l + (x[i] - u) % ρ
+        end
+
+    end
+    x
+end
+
+function reset_to_violated_bounds!(x, bounds)
+    for i in 1:size(bounds,2)
+        l = bounds[1, i]
+        u = bounds[2, i]
+        if l > x[i]
+            x[i] = l
+        elseif x[i] > u 
+            x[i] = u
+        end
+
+    end
+    x 
+end
+
+function evo_boundary_repairer!(x, x_best, bounds)            
+    for i in 1:size(bounds,2)
+        l = bounds[1, i]
+        u = bounds[2, i]
+        if l > x[i]
+            α = rand()
+            x[i] =  α*l + (1.0 - α) * x_best[i]
+        elseif x[i] > u  
+            β = rand()
+            x[i] =  β*u + (1.0 - β) * x_best[i]
+        end
+
+    end
+    x  
+end
 ###################################################
 #      Solutions and population functions
 #          for Matrix representation
@@ -189,12 +257,12 @@ function countViolations(g::Vector, h::Vector)
 
     for i = 1:length(g)
         if g[i] > 0
-            sum_g += 1  end
+        sum_g += 1  end
     end
 
     for i = 1:length(h)
         if h[i] != 0.0
-            sum_h += 1  end
+        sum_h += 1  end
     end
 
     return sum_g + sum_h
