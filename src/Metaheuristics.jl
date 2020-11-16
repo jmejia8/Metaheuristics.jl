65100e850fde40d9d33b6926b4be576b9552a11b
Jesus Mejia <jesusmejded@gmail.com>
2020-11-16 13:14:35 -0600

    refactor file structure

--- src/Metaheuristics.jl
+++ src/Metaheuristics.jl
@@ -13,70 +13,81 @@ export MOEAD_DE
 export convergence, minimizer, minimum, positions, fvals, nfes, get_position, fval, NSGA2
 
 include("externals.jl")
-include("stop.jl")
-include("structures.jl")
-include("operators.jl")
-include("display.jl")
-include("algorithm.jl")
-include("tools.jl")
-include("multi-objective-functions.jl")
+include("common/stop.jl")
+include("common/structures.jl")
+include("common/operators.jl")
+include("solutions/display.jl")
+include("algorithms/algorithm.jl")
+include("common/tools.jl")
+include("common/multi-objective-functions.jl")
 
 include("optimize.jl")
 
+#######################################################
+#                                                     #
+#           A L G O R I T H M S                       #        
+#                                                     #
+#######################################################
+
 # ECA algorithm
-include("eca.jl")
+include("algorithms/ECA/ECA.jl")
 
 # Differential algorithm
-include("diffEvolution.jl")
+include("algorithms/DE/DE.jl")
 
 # PSO algorithm
-include("pso.jl")
+include("algorithms/PSO/PSO.jl")
 
 # The whale optimization algorithm
 # S Mirjalili, A Lewis - Advances in Engineering Software, 2016
-include("WOA.jl")
-
-# Grasshopper optimisation algorithm: Theory and application
-# S Saremi, S Mirjalili, A Lewis - Advances in Engineering Software, 2017
-include("GOA.jl")
-
-# GSA: a gravitational search algorithm
-# E Rashedi, H Nezamabadi-Pour, S Saryazdi - Information sciences, 2009
-include("GSA.jl")
+include("algorithms/WOA/WOA.jl")
 
 # Mirjalili, Seyedali, and Amir H. Gandomi.
 # "Chaotic gravitational constants for the gravitational search algorithm."
 # Applied Soft Computing 53 (2017): 407-419.
-include("CGSA.jl")
+include("algorithms/CGSA/CGSA.jl")
 
 # SA: Simulated Annealing
 # Kirkpatrick, S., Gelatt, C.D., & Vecchi, M.P. (1983). Optimization by
 # Simulated Annealing. _Science, 220_, 671-680.
-include("SA.jl")
+include("algorithms/SA/SA.jl")
+
+
+# Aritifical Bee colony
+include("algorithms/ABC/ABC.jl")
+
+# MOEA decomposition based using Differential Evolution
+include("algorithms/MOEAD_DE/MOEAD_DE.jl")
+
+# Non-dominate sorting Genetic Algorithm
+include("algorithms/NSGA2/NSGA2.jl")
+
+# Grasshopper optimisation algorithm: Theory and application
+# S Saremi, S Mirjalili, A Lewis - Advances in Engineering Software, 2017
+include("deprecated/GOA.jl")
+
+# GSA: a gravitational search algorithm
+# E Rashedi, H Nezamabadi-Pour, S Saryazdi - Information sciences, 2009
+include("deprecated/GSA.jl")
 
 # Li, Zhenhua, and Qingfu Zhang.
 # "An efficient rank-1 update for Cholesky CMA-ES using auxiliary evolution path."
 # Evolutionary Computation (CEC), 2017 IEEE Congress on. IEEE, 2017.
-include("CMAES_AEP.jl")
+include("deprecated/CMAES_AEP.jl")
 
 # R.A. Formato Central force optimization:
 # a new metaheuristic with applications in applied electromagnetics
 # Progress in Electromagnetics Research, PIER 77 (2007), pp. 425-491, 10.2528/PIER07082403
-include("CFO.jl")
+include("deprecated/CFO.jl")
 
 # Brest, J., Maučec, M. S., & Bošković, B. (2017, June).
 # Single objective real-parameter optimization: Algorithm jSO.
 # In Evolutionary Computation (CEC), 2017 IEEE Congress on (pp. 1311-1318). IEEE.
-include("jso.jl")
+include("deprecated/jso.jl")
 
 
-include("ABC.jl")
-
-include("moead-de.jl")
-
-include("NSGA2.jl")
-
-include("deprecated.jl")
+# deprecated functions/optimizers/etc
+include("deprecated/deprecated.jl")
 
 
 end # module
