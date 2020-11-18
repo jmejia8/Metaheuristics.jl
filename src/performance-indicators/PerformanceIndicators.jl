6246d8153a2430bc487e91a07783128e9d112ecd
Jesus Mejia <jesusmejded@gmail.com>
2020-11-18 01:09:50 -0600

    Performance indicators are working

--- src/performance-indicators/PerformanceIndicators.jl
+++ src/performance-indicators/PerformanceIndicators.jl
@@ -1,6 +1,68 @@
+
+"""
+	PerformanceIndicators
+
+This module includes performance indicators to assess evolutionary multi-objective
+optimization algorithms.
+
+- `gd` Generational Distance
+- `igd` Inverted Generational Distance
+- `gd_plus` Generational Distance plus
+- `igd_plus` Inverted Generational Distance plus
+
+### Example
+
+```julia
+julia> import Metaheuristics: PerformanceIndicators, TestProblems
+
+julia> A = [ collect(1:3) collect(1:3) ]
+3×2 Array{Int64,2}:
+ 1  1
+ 2  2
+ 3  3
+
+julia> B = A .- 1
+3×2 Array{Int64,2}:
+ 0  0
+ 1  1
+ 2  2
+
+julia> PerformanceIndicators.gd(A, B)
+0.47140452079103173
+
+julia> f, bounds, front = TestProblems.get_problem(:ZDT1);
+
+julia> front
+                          F space
+         ┌────────────────────────────────────────┐ 
+       1 │⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠈⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠈⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠈⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠉⠢⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+   f_2   │⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠲⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠒⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠢⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+         │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⢤⣀⠀⠀⠀⠀⠀│ 
+       0 │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠢⢄⣀│ 
+         └────────────────────────────────────────┘ 
+         0                                        1
+                            f_1
+
+julia> PerformanceIndicators.igd_plus(front, front)
+0.0
+```
+
+
+"""
 module PerformanceIndicators
 
-import ..State, ..fvals, ..norm
+import ..State, ..fvals, ..norm, ..xFgh_indiv
 
 """
 	generational_distance(front, true_pareto_front; p = 1, inverted = false, plus=false)
@@ -9,7 +71,10 @@ Returns the Generational Distance.
 
 ### Parameters
 
-- `front` is `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+- `front` and `true_pareto_front` can be:
+	- `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+	- `State`
+	- `Array{xFgh_indiv}` (usually `State.population`)
 - `true_pareto_front` is a `M×m` matrix.
 - `p` is the power in for the ‖⋅‖_p
 - `plus` if true then computes the GD+
@@ -48,6 +113,26 @@ function generational_distance(front, true_pareto_front; p = 1, inverted = false
 end
 
 
+function generational_distance(front::Union{Array{xFgh_indiv}, State},
+						       true_pareto_front::Union{Array{xFgh_indiv}, State};
+								p = 1,
+								inverted = false,
+								plus = false
+		)
+	generational_distance(fvals(front), fvals(true_pareto_front); p=p,inverted=inverted,plus=plus)
+end
+
+
+function generational_distance(front::Union{Array{xFgh_indiv}, State},
+						       true_pareto_front;
+								p = 1,
+								inverted = false,
+								plus = false
+		)
+	generational_distance(fvals(front), (true_pareto_front); p=p,inverted=inverted,plus=plus)
+end
+
+
 """
 	gd(front, true_pareto_front; p = 1)
 
@@ -55,11 +140,12 @@ Returns the Generational Distance.
 
 ### Parameters
 
-- `front` is `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
-- `true_pareto_front` is a `M×m` matrix.
+`front` and `true_pareto_front` can be:
+	- `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+	- `State`
+	- `Array{xFgh_indiv}` (usually `State.population`)
 """
 gd(front, true_pareto_front; p = 1) = generational_distance(front, true_pareto_front, p=p)
-gd(front::State, true_pareto_front::State; p = 1) = gd(fvals(front), fvals(true_pareto_front), p=p)
 
 
 """
@@ -69,11 +155,12 @@ Returns the Inverted Generational Distance.
 
 ### Parameters
 
-- `front` is `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
-- `true_pareto_front` is a `M×m` matrix.
+`front` and `true_pareto_front` can be:
+	- `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+	- `State`
+	- `Array{xFgh_indiv}` (usually `State.population`)
 """
 igd(front, true_pareto_front; p = 1) = generational_distance(front, true_pareto_front, inverted=true, p=p)
-igd(front::State, true_pareto_front::State; p = 1) = igd(fvals(front), fvals(true_pareto_front), p=p)
 
 """
 	gd_plus(front, true_pareto_front; p = 1)
@@ -82,11 +169,12 @@ Returns the Generational Distance Plus.
 
 ### Parameters
 
-- `front` is `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
-- `true_pareto_front` is a `M×m` matrix.
+`front` and `true_pareto_front` can be:
+	- `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+	- `State`
+	- `Array{xFgh_indiv}` (usually `State.population`)
 """
 gd_plus(front, true_pareto_front; p = 1) = generational_distance(front, true_pareto_front, p=p, plus=true)
-gd_plus(front::State, true_pareto_front::State; p = 1) = igd_plus(fvals(front), fvals(true_pareto_front), p=p)
 
 """
 	igd_plus(front, true_pareto_front; p = 1)
@@ -95,11 +183,12 @@ Returns the Inverted Generational Distance Plus.
 
 ### Parameters
 
-- `front` is `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
-- `true_pareto_front` is a `M×m` matrix.
+`front` and `true_pareto_front` can be:
+	- `N×m` matrix where `N` is the number of points and `m` is the number of objectives. 
+	- `State`
+	- `Array{xFgh_indiv}` (usually `State.population`)
 """
 igd_plus(front, true_pareto_front; p = 1) = generational_distance(front, true_pareto_front, inverted=true, p=p, plus=true)
-igd_plus(front::State, true_pareto_front::State; p = 1) = igd_plus(fvals(front), fvals(true_pareto_front), p=p)
 
 
 end
