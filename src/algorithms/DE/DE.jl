16f74cc82517fcc9d7306a5a2123b1dd11fd6559
Jesus Mejia <jesusmejded@gmail.com>
2020-11-20 19:17:49 -0600

    minor update

--- src/algorithms/DE/DE.jl
+++ src/algorithms/DE/DE.jl
@@ -1,3 +1,5 @@
+include("crossover_mutation.jl")
+
 mutable struct DE
     N::Int
     F::Float64
@@ -95,7 +97,6 @@ function update_state_de!(
     F = parameters.F
     CR = parameters.CR
 
-    D = size(problem.bounds, 2)
 
     # stepsize
     if parameters.F_min < parameters.F_max
@@ -110,93 +111,16 @@ function update_state_de!(
     N = parameters.N
     strategy = parameters.strategy
 
-    la = problem.bounds[1, :]
-    lb = problem.bounds[2, :]
-
-    D = length(la)
-
     xBest = status.best_sol.x
 
     for i = 1:N
 
-        # select participats
-        r1 = rand(1:N)
-        while r1 == i
-            r1 = rand(1:N)
-        end
-
-        r2 = rand(1:N)
-        while r2 == i || r1 == r2
-            r2 = rand(1:N)
-        end
-
-        r3 = rand(1:N)
-        while r3 == i || r3 == r1 || r3 == r2
-            r3 = rand(1:N)
-        end
-
-        x = currentPop[i].x
-        a = currentPop[r1].x
-        b = currentPop[r2].x
-        c = currentPop[r3].x
-
-        # strategy is selected here
-        if strategy == :rand1
-            # DE/rand/1
-            u = a + F * (b - c)
-        elseif strategy == :best1
-            # DE/best/1
-            u = xBest + F * (b - c)
-        elseif strategy == :rand2
-            # DE/rand/2
-
-            r4 = rand(1:N)
-            while r4 == i || r4 == r1 || r4 == r2 || r4 == r3
-                r4 = rand(1:N)
-            end
-
-            r5 = rand(1:N)
-            while r5 == i || r5 == r1 || r5 == r2 || r5 == r3 || r5 == r4
-                r5 = rand(1:N)
-            end
-
-            d = currentPop[r4].x
-            ee = currentPop[r5].x
-
-            u = ee + F * (a - b + c - d)
-        elseif strategy == :randToBest1
-            # DE/rand-to-best/1
-            u = x + F * (xBest - x + a - b)
-        elseif strategy == :best2
-            # DE/best/2
-            r4 = rand(1:N)
-            while r4 == i || r4 == r1 || r4 == r2 || r4 == r3 || r4 == best_ind
-                r4 = rand(1:N)
-            end
-            d = currentPop[r4].x
-            u = xBest + F * (a - b + c - d)
+        if strategy == :randToBest1
+            u = DE_mutation(currentPop, i, F, strategy, xBest)
         else
-            @error("Unknown strategy $(strategy)")
+            u = DE_mutation(currentPop, i, F, strategy)
         end
-
-        # binomial crossover
-        v = zeros(D)
-        j_rand = rand(1:D)
-
-        # binomial crossover
-        for j = 1:D
-            if rand() < CR || j == j_rand
-                v[j] = u[j]
-
-                if v[j] < la[j]
-                    v[j] = la[j]
-                elseif v[j] > lb[j]
-                    v[j] = lb[j]
-                end
-            else
-                v[j] = x[j]
-            end
-        end
+        v = DE_crossover(currentPop[i].x, u, CR)
 
         # instance child
         v = evo_boundary_repairer!(v, xBest, problem.bounds)
