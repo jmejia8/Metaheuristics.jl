1bb98cd643a6221bfbc7199689d6cc79c0ed10fc
Jesus Mejia <jesusmejded@gmail.com>
2021-09-17 13:35:34 -0500

    import linear algebra methods

--- src/externals.jl
+++ src/externals.jl
@@ -1,6 +1,7 @@
 if VERSION >= v"0.7.0"
 	import Random: randperm, shuffle!, shuffle, seed!
 	import Printf.@printf
+	import LinearAlgebra
 	import LinearAlgebra: norm, Diagonal, dot
 	import Statistics: var, mean, std
 	using UnicodePlots
